577a22d4dd25f0d2b97ed60b90ecb5fda85d195e
Shaun Russell <shaun.d.russell@gmail.com>
2013-03-03 16:30:27 -0800

    rails update
    readme
    
    readme

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 rails3_boilerplate
 ==================
 
-rspec, haml, sass, bootstrap, font awesome, compass, devise
+haml, sass, bootstrap, font awesome, compass, devise, rspec
+
+* ruby 2.0
+* all devise views converted to haml/bootstrap
+* devise scoped views (:users)
+* flash notices setup for bootstrap
+* devise notices setup for bootstrap
+
+to use:
+
+* clone as new project name
+* create new repo on github
+* change remote in .git/config of clone to that of new repo
+* push origin/master
+* find/replace all instances of boilerplate with your project name
+
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Boilerplate::Application.config.session_store :cookie_store, key: '_askyourself_session'
+Boilerplate::Application.config.session_store :cookie_store, key: '_boilerplate_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# Askyourself::Application.config.session_store :active_record_store
+# Boilerplate::Application.config.session_store :active_record_store
